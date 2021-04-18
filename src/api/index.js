ea02c7f64be9ca189da6b8b7e7a03cdc01972d0e
Regan Barua <reganbarua2015@gmail.com>
2021-04-18 12:55:56 -0700

    like funtionality added

--- src/api/index.js
+++ src/api/index.js
@@ -10,3 +10,5 @@ export const updatePost = (id, updatedPost) =>
   Axios.patch(`${url}/${id}`, updatedPost);
 
 export const deletePost = (id) => Axios.delete(`${url}/${id}`);
+
+export const likePost = (id) => Axios.patch(`${url}/${id}/likePost`);
--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -14,7 +14,7 @@ import moment from "moment";
 import { useDispatch } from "react-redux";
 
 import useStyles from "./styles";
-import { deletePost } from "../../../actions/posts";
+import { deletePost, likePost } from "../../../actions/posts";
 
 const Post = ({ post, setCurrentId }) => {
   console.log(post._id);
@@ -61,7 +61,11 @@ const Post = ({ post, setCurrentId }) => {
         </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => dispatch(likePost(post._id))}
+        >
           <ThumbUpAltIcon fontSize="small" /> Like {post.likeCount}{" "}
         </Button>
         <Button
